ddade192bbded7d7353f97a3ec4190abd2a780f5
Lance A. Endres <lendres@fifthrace.com>
2024-11-12 07:25:16 -0600

    Basic functionality working.

---
+++ TestLibrary/AppHostBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace TestLibrary;
+
+public static class AppHostBuilderExtensions
+{
+	public static MauiAppBuilder UseTestLibrary(this MauiAppBuilder builder)
+	{
+		builder.Services.AddSingleton<TestLibrary.Resources.Styles.LibraryColors>();
+		return builder;
+	}
+}
